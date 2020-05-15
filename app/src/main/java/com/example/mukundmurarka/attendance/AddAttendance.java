6d588feebdb10794adc026bb9f8464288d93c372
Mukundmurarka <mukundmurarka1996@gmail.com>
2020-05-15 22:44:00 +0530

    fifth

---
+++ app/src/main/java/com/example/mukundmurarka/attendance/AddAttendance.java
@@ -0,0 +1,14 @@
+
+package com.example.mukundmurarka.attendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddAttendance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_attendance);
+    }
+}
--- app/src/main/java/com/example/mukundmurarka/attendance/Teacheractivity.java
+++ app/src/main/java/com/example/mukundmurarka/attendance/Teacheractivity.java
@@ -1,13 +1,47 @@
 package com.example.mukundmurarka.attendance;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Teacheractivity extends AppCompatActivity {
+    Button view, add, total;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacheractivity);
+        view = (Button)findViewById(R.id.viewatt);
+        add = (Button)findViewById(R.id.addatt);
+        total =  (Button)findViewById(R.id.totalatt);
+
+
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(Teacheractivity.this,Viewattendance.class);
+                startActivity(intent1);
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(Teacheractivity.this,AddAttendance.class);
+                startActivity(intent2);
+
+            }
+        });
+        total.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3 =  new Intent(Teacheractivity.this,TotalAttendance.class);
+                startActivity(intent3);
+            }
+        });
     }
 }
